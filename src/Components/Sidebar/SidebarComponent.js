665732c85776285e4e2b75700e056065c5a74eb5
Dikhit <dikhitbhuyan@gmail.com>
2019-09-19 20:10:49 +0530

    laptop hanged at 25.30

--- src/Components/Sidebar/SidebarComponent.js
+++ src/Components/Sidebar/SidebarComponent.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import debounce from '../Helpers/Helpers';
+import { withStyles } from '@material-ui/core/styles';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import Styles from './EditorStyle';
 
 class SidebarComponent extends React.Component{
     render(){
         return(
             <React.Fragment>
-
+                <div>hello from side bar</div>
             </React.Fragment>
         );
     }
 }
 
-export default SidebarComponent;
+export default withStyles(styles)(SidebarComponent);
--- src/Components/SidebarItem/SidebarItemComponent.js
+++ src/Components/SidebarItem/SidebarItemComponent.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './SidebarStyle';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { removeHTMLTags } from '../Helpers/Helpers';
 
 class SidebarItemComponent extends React.Component{
     render(){
